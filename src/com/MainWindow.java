4312f1204f05afa3dfa2ccf0c947272d135bcdb1
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-30 22:39:35 +0500

    create base window

---
+++ src/com/MainWindow.java
@@ -0,0 +1,24 @@
+package com;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,245);
+        setLocation(400,400);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        MainWindow mainWindow = new MainWindow();
+
+    }
+
+
+}
